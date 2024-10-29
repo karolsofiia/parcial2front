c2c10a4a826c0f00822a52d132a75fa244b61529
karolsofiia <alvarezkarolsofia61@gmail.com>
2024-10-29 08:08:57 -0500

    Update Signup.jsx

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -19,7 +19,7 @@ export default function Signup({ role }) {
 
 
     try {
-      const response = await fetch('https://parcial2-back.vercel.app/user/signup', {
+      const response = await fetch('https://ganacomoloco-back.vercel.app/user/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
